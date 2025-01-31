cfa675cc2a76eb6f160068754cbc5d7ade851f2c
yojanasoni836 <yojanasoni836@gmail.com>
2025-01-31 09:01:36 +0530

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Greeting{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name = scanner.nextLine();
+        System.out.println("Hello"+ name +",Have a good day!");
+        scanner.close();
+        
+    }
+}
