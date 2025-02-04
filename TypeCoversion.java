d872a0a07015a2e95a293a5490e67ff6d123367c
yojanasoni836 <yojanasoni836@gmail.com>
2025-02-04 19:52:46 +0530

    Add files via upload

---
+++ TypeCoversion.java
@@ -0,0 +1,8 @@
+public class TypeCoversion{
+    public static void main(String[]args){
+        int intValue = 200;
+        double doubleValue = intValue;
+        System.out.println("Integer Value:"+intValue);
+        System.out.println("Double Value:"+doubleValue);
+    }
+}
