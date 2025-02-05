c9499a9a84d48cdc02d0f0a9d31947d80c3284f2
yojanasoni836 <yojanasoni836@gmail.com>
2025-02-05 17:32:31 +0530

    Add files via upload

---
+++ Coversion.java
@@ -0,0 +1,9 @@
+public class Coversion{
+    public static void main(String[]args){
+        float floatValue=200f;
+        int intValue=(int)floatValue;
+        System.out.println("Original float value:" + floatValue);
+        System.out.println("Converted integer value:" + intValue);
+        
+    }
+}
